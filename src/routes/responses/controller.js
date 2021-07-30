0258d1a0ce95acf5e9ac3bf9fb92e8f1437e39a9
jpaltahona <jaltahona@lean-tech.io>
2021-07-30 01:36:56 -0500

    settings

--- src/routes/responses/controller.js
+++ src/routes/responses/controller.js
@@ -5,7 +5,19 @@ import {getDataUser} from '../../logic/getStudent';
 export const getResponseTeacher = async(req, res) => {
   const id = req.params.id;
   const teacherData = await modelRespueta.find({ docente: id });
-  res.status(200).json(teacherData)
+
+  let returnResponses = [];
+  for await (let i of teacherData) {
+    let arrayIds = [];
+      for await (let e of i.respuestas) {
+        arrayIds.push(e.ref);
+      }
+    const respuestas = await componentsSchema.find({ "_id": { $in: arrayIds } });
+    i.respuestas = respuestas;
+    returnResponses.push(i);
+  };
+
+  res.status(200).json(returnResponses)
 }
 
 
