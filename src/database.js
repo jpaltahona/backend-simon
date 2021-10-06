712fba4d572d36fc7b4da6e9d5f39fd3d9fa647d
jpaltahona <jpaltahonacc@gmail.com>
2021-10-06 09:01:08 -0500

    settings

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb://localhost/encuesta", {
+mongoose.connect("mongodb://localhost/simonDataBases", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
